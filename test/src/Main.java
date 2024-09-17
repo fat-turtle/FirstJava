d6683c7b2fa5c8f2effb8df75c3f003c4cb880ea
Luca <lucatheboss2016@gmail.com>
2024-09-16 20:56:58 -0400

    first commit

---
+++ test/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("dog test");
+    dog firstDog = new dog("pourity", 7);
+    System.out.println(firstDog.name + " " + firstDog.age);
+    }
+}
---
+++ test/src/dog.java
@@ -0,0 +1,9 @@
+public class dog {
+    String name;
+    int age;
+
+    public dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
